2788342060f6cb5fee4c1b35a9903bc4a87229f4
dokkalemon <dottori.roberto@gmail..com>
2021-11-10 15:29:56 +0100

    add generation numb

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-console.log(('JS OK'));
+/* 
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri in pagina devono essere rimossi e l’utente deve inserire, 
+uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri 
+da indovinare sono stati individuati.
+*/
+
+
+//HTML ref
+const numbConteiner = document.querySelector('.number-conteiner');
+
+//costruiamo la struttura
+for (let i = 0; i < 5; i++) {
+    const numberItems = document.createElement('div');
+    numberItems.classList.add('number-items');
+    numbConteiner.append(numberItems);
+
+    const numbP = document.createElement('p');
+    numberItems.append(numbP);
+
+    numbP.innerHTML += genRand()
+
+}
+
+
+
+
+
+
+
+
+//-----------------FUNCTION----------------//
+//generiamo numeri random
+
+function genRand() {
+    return Math.floor( Math.random() * 100 + 1)
+}
+
+console.log(genRand());
+
+
